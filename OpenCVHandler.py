8edc1b89b3a9f55ddb283615e1e8f99e81dd20da
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-28 12:47:47 +0100

    .

--- OpenCVHandler.py
+++ OpenCVHandler.py
@@ -30,23 +30,23 @@ class OpenCVHandler(threading.Thread):
 
         self.num_detected = 0
 
-    def detect_people( self, frame ):
-    	(rects, weights) = hog.detectMultiScale( frame, winStride=(8, 8), padding=(16, 16), scale=1.06 )
-    	rects = non_max_suppression( rects, probs=None, overlapThresh=0.65 )
-    	for (x, y, w, h) in rects:
-    		cv2.rectangle( frame, (x, y), (x + w, y + h), (0, 0, 255), 2 )
-        return (frame, len(rects))
-
-    def background_subtraction( self, previous_frame, frame, min_area):
-    	frameDelta = cv2.absdiff( previous_frame, frame )
-    	thresh = cv2.threshold( frameDelta, 25, 255, cv2.THRESH_BINARY )[1]
-    	thresh = cv2.dilate( thresh, None, iterations=2 )
-    	im2, cnts, hierarchy = cv2.findContours( thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE )
-    	temp=0
-    	for c in cnts:
-    		if cv2.contourArea(c) > min_area:
-    			temp=1
-        return temp
+    # def detect_people( self, frame ):
+    # 	(rects, weights) = hog.detectMultiScale( frame, winStride=(8, 8), padding=(16, 16), scale=1.06 )
+    # 	rects = non_max_suppression( rects, probs=None, overlapThresh=0.65 )
+    # 	for (x, y, w, h) in rects:
+    # 		cv2.rectangle( frame, (x, y), (x + w, y + h), (0, 0, 255), 2 )
+    #     return (frame, len(rects))
+    #
+    # def background_subtraction( self, previous_frame, frame, min_area):
+    # 	frameDelta = cv2.absdiff( previous_frame, frame )
+    # 	thresh = cv2.threshold( frameDelta, 25, 255, cv2.THRESH_BINARY )[1]
+    # 	thresh = cv2.dilate( thresh, None, iterations=2 )
+    # 	im2, cnts, hierarchy = cv2.findContours( thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE )
+    # 	temp=0
+    # 	for c in cnts:
+    # 		if cv2.contourArea(c) > min_area:
+    # 			temp=1
+    #     return temp
 
     def run( self ):
         self.running = True
@@ -60,30 +60,30 @@ class OpenCVHandler(threading.Thread):
             image = imutils.resize( image, width=min(400, image.shape[1] ))
             image_grayscale = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY )
 
-            image_processes, self.num_detected = detect_people( image )
+            # image_processes, self.num_detected = detect_people( image )
 
-            cv2.imwrite('image.jpg',image)
-            cv2.imwrite('image_processes.jpg',image_processes)
+            # detect people in the image
+            (rects, weights) = self.hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
-            # # detect people in the image
-            # (rects, weights) = self.hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
-            #
-            # # apply non-maxima suppression to the bounding boxes using a
-            # # fairly large overlap threshold to try to maintain overlapping
-            # # boxes that are still people
-            # rects = numpy.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-            # pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
-            #
-            # # draw the final bounding boxes
-            # for (xA, yA, xB, yB) in pick:
-            #     cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
-            #
-            # # save number of people detected
-            # self.num_detected = len(pick)
+            # apply non-maxima suppression to the bounding boxes using a
+            # fairly large overlap threshold to try to maintain overlapping
+            # boxes that are still people
+            rects = numpy.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+            pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+            # draw the final bounding boxes
+            for (xA, yA, xB, yB) in pick:
+                cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+            # save number of people detected
+            self.num_detected = len(pick)
 
             # show the output images
             #  cv2.imshow("After NMS", image)
             # key = cv2.waitKey(1) & 0xFF
+
+            cv2.imwrite('image.jpg',image)
+            # cv2.imwrite('image_processes.jpg',image_processes)
 
             # clear the stream in preparation for the next frame
             self.rawCapture.truncate(0)
