dfdb68cd4ed15af6bcc6a06f38202eb36cd5d3b6
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-28 13:50:03 +0100

    .

--- OpenCVHandler_back.py
+++ OpenCVHandler_back.py
@@ -54,37 +54,45 @@ class OpenCVHandler(threading.Thread):
         time.sleep(0.1)
 
         frame_num = 0
+        image_grayscale = None
+        previous_image_grayscale = None
 
         for frame in self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
+            previous_image_grayscale = image_grayscale
             image = frame.array
             image = imutils.resize( image, width=min(640, image.shape[1] ))
             image_grayscale = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY )
 
-            # detect people in the image
-            (rects, weights) = self.hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+            if( previous_image_grayscale is not None ):
 
-            # apply non-maxima suppression to the bounding boxes using a
-            # fairly large overlap threshold to try to maintain overlapping
-            # boxes that are still people
-            rects = numpy.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-            pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+                min_area=(3000/640)*image.shape[1]
+                if( self.background_subtraction( previous_image_grayscale, image_grayscale, min_area ) ):
 
-            # draw the final bounding boxes
-            for (xA, yA, xB, yB) in pick:
-                cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+                    # detect people in the image
+                    (rects, weights) = self.hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
-            # save number of people detected
-            self.num_detected = len(pick)
+                    # apply non-maxima suppression to the bounding boxes using a
+                    # fairly large overlap threshold to try to maintain overlapping
+                    # boxes that are still people
+                    rects = numpy.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+                    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
-            # show the output images
-            #  cv2.imshow("After NMS", image)
-            # key = cv2.waitKey(1) & 0xFF
+                    # draw the final bounding boxes
+                    for (xA, yA, xB, yB) in pick:
+                        cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
-            filename = "image" + str(frame_num) + ".jpg"
-            cv2.imwrite(filename,image)
-            # cv2.imwrite('image_processes.jpg',image_processes)
+                    # save number of people detected
+                    self.num_detected = len(pick)
 
-            print( "People detected(" + str(frame_num) + "): " + str(self.num_detected) )
+                    # show the output images
+                    #  cv2.imshow("After NMS", image)
+                    # key = cv2.waitKey(1) & 0xFF
+
+                    filename = "image" + str(frame_num) + ".jpg"
+                    cv2.imwrite(filename,image)
+                    # cv2.imwrite('image_processes.jpg',image_processes)
+
+                    print( "People detected(" + str(frame_num) + "): " + str(self.num_detected) )
 
             # clear the stream in preparation for the next frame
             self.rawCapture.truncate(0)
