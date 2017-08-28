aabd180021dfc039fc40112d93e370e3b541c33d
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-28 13:42:44 +0100

    .

--- OpenCVHandler.py
+++ OpenCVHandler.py
@@ -38,14 +38,14 @@ class OpenCVHandler(threading.Thread):
     #     return (frame, len(rects))
 
     def background_subtraction( self, previous_frame, frame, min_area):
-    	frameDelta = cv2.absdiff( previous_frame, frame )
-    	thresh = cv2.threshold( frameDelta, 25, 255, cv2.THRESH_BINARY )[1]
-    	thresh = cv2.dilate( thresh, None, iterations=2 )
-    	im2, cnts, hierarchy = cv2.findContours( thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE )
-    	temp=0
-    	for c in cnts:
-    		if cv2.contourArea(c) > min_area:
-    			temp=1
+        frameDelta = cv2.absdiff( previous_frame, frame )
+        thresh = cv2.threshold( frameDelta, 25, 255, cv2.THRESH_BINARY )[1]
+        thresh = cv2.dilate( thresh, None, iterations=2 )
+        im2, cnts, hierarchy = cv2.findContours( thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE )
+        temp=0
+        for c in cnts:
+            if cv2.contourArea(c) > min_area:
+                temp=1
         return temp
 
     def run( self ):
