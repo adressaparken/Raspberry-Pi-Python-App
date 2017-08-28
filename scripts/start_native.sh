a35814e5d7fce2a6905f98d69de173a36aea9b0b
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-28 16:36:58 +0100

    .

--- scripts/start_native.sh
+++ scripts/start_native.sh
@@ -11,7 +11,7 @@ while true; do
     echo "Checking for updates"
     sh $SCRIPT_DIR/pull_git_native.sh
     echo "Restarting native app!"
-    python $ROOT_DIR/$NATIVE_APP.py
+    python3 $ROOT_DIR/$NATIVE_APP.py
     echo "Main app ended..."
     echo "-----------------------------------"
     read -t 10 -p "Restarting the native app in 10 seconds. Press <CTRL>+c to stop."
