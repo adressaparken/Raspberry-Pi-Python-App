b90e67b885ac66051e4d8c96fa16dc0a61ceb05d
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-28 16:35:38 +0100

    .

--- OpenCVHandler.py
+++ OpenCVHandler.py
@@ -50,24 +50,16 @@ class OpenCVHandler(threading.Thread):
         frame_num = 0
 
         for frame in self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
-            start_time = current_milli_time()
-
             image = frame.array
-
-            log_info("Grabbed image: " + str(current_milli_time()-start_time))
 
             # detect people in the image
             (rects, weights) = self.hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
-
-            log_info("Detect People: " + str(current_milli_time()-start_time))
 
             # apply non-maxima suppression to the bounding boxes using a
             # fairly large overlap threshold to try to maintain overlapping
             # boxes that are still people
             rects = numpy.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
             pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
-
-            log_info("Pick: " + str(current_milli_time()-start_time))
 
             # draw the final bounding boxes
             for (xA, yA, xB, yB) in pick:
@@ -84,12 +76,10 @@ class OpenCVHandler(threading.Thread):
             # cv2.imwrite(filename,image)
             # cv2.imwrite('image_processes.jpg',image_processes)
 
-            print( "People detected(" + str(frame_num) + "): " + str(self.num_detected) )
+            # print( "People detected(" + str(frame_num) + "): " + str(self.num_detected) )
 
             # clear the stream in preparation for the next frame
             self.rawCapture.truncate(0)
-
-            log_info("Finished: " + str(current_milli_time()-start_time))
 
             frame_num += 1
 
